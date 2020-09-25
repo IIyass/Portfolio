9329d4653be324337cc5dacb227245834f7bd7d1
iiyass <proilyass@outlook.fr>
2020-09-25 17:11:31 +0100

    Fixing ProjectPage Bug

--- src/pages/project.js
+++ src/pages/project.js
@@ -4,47 +4,52 @@ import { Link, graphql, useStaticQuery } from "gatsby"
 
 import ProjectCard from "../components/Utils/ProjectCard"
 import * as Style from "../styles"
+
 const Blog = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      github {
-        viewer {
-          repositories(
-            first: 8
-            orderBy: { field: STARGAZERS, direction: DESC }
-          ) {
-            edges {
-              node {
-                id
-                name
-                url
-                description
-              }
-            }
-          }
-        }
-      }
-    }
-  `)
-  return (
-    <Layout>
-      <h1>Projects</h1>
-      <Style.ProjectLayout>
-        {data.github.viewer.repositories.edges.map(project => {
-          //FOREACH TEST =?EXCO 1 RESULT
-          // COLOR BLINDNESS // instgram program
-          return (
-            <ProjectCard
-              id={project.node.id}
-              name={project.node.name}
-              url={project.node.url}
-              description={project.node.description}
-              url={project.node.url}
-            />
-          )
-        })}
-      </Style.ProjectLayout>
-    </Layout>
-  )
+  return <>Hello</>
 }
+
+// const Blog = () => {
+//   const data = useStaticQuery(graphql`
+//     query {
+//       github {
+//         viewer {
+//           repositories(
+//             first: 8
+//             orderBy: { field: STARGAZERS, direction: DESC }
+//           ) {
+//             edges {
+//               node {
+//                 id
+//                 name
+//                 url
+//                 description
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `)
+//   return (
+//     <Layout>
+//       <h1>Projects</h1>
+//       <Style.ProjectLayout>
+//         {data.github.viewer.repositories.edges.map(project => {
+//           //FOREACH TEST =?EXCO 1 RESULT
+//           // COLOR BLINDNESS // instgram program
+//           return (
+//             <ProjectCard
+//               id={project.node.id}
+//               name={project.node.name}
+//               url={project.node.url}
+//               description={project.node.description}
+//               url={project.node.url}
+//             />
+//           )
+//         })}
+//       </Style.ProjectLayout>
+//     </Layout>
+//   )
+// }
 export default Blog
