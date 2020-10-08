1471dffa08c75ff8662df4bd0823ae00b7cfcdbb
iiyass <proilyass@outlook.fr>
2020-10-08 02:57:54 +0100

    Fixing prod bug

--- src/pages/model.js
+++ src/pages/model.js
@@ -2,7 +2,7 @@ import React from "react"
 import ModelComponent from "../components/Utils/Modal/Modal"
 import { Global } from "../components/layout/styles"
 const Model = props => {
-  const { message, picture } = props.location.state
+  const { message, picture } = props.location.state || ""
 
   return (
     <>
