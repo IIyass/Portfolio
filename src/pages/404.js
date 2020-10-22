9849ad17701b25058c56cf85c1836d774c534425
iiyass <ilyass1.ib@outlook.com>
2020-10-22 01:19:27 +0100

    testing git

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 
 const Notfound = () => {
-  return <Layout>Not found Page</Layout>
+  return <Layout>Not found Page :?</Layout>
 }
 
 export default Notfound
